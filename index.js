7d6500b88b300b821acfd89b7f04916424fbe229
Simon <scwong93@gmail.com>
2018-07-31 17:34:52 -0700

    created html and js file

---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+
+function showPopularMovies() {
+  fetch("https://api.themoviedb.org/3/discover/tv?api_key=fb6a1d3f38c3d97f67df6d141f936f29&language=en-US&sort_by=popularity.desc&page=1&format=json")
+    .then(function (response) {
+      return response.json();
+    }).then(function (responseText) {
+      if (responseText) {
+        let movieList = document.getElementById('movie-list');
+        let results = responseText.results;
+        results.map(movie => {
+          let li = document.createElement('li');
+          li.innerHTML = movie.original_name;
+          movieList.append(li);
+        })
+      }
+    })
+}
+
+function searchMovies() {
+  let input = document.getElementById('searchBar').value;
+  if (input) {
+    fetch(`https://api.themoviedb.org/3/search/tv?api_key=fb6a1d3f38c3d97f67df6d141f936f29&language=en-US&query=${input}&page=1&format=json`)
+      .then(function (response) {
+        return response.json();
+      }).then(function (responseText) {
+        if (responseText) {
+          let movieList = document.getElementById('movie-list');
+          let results = responseText.results;
+          console.log(results);
+          results.map(movie => {
+            let movieId = movie.id;
+            let a = document.createElement('a');
+            let li = document.createElement('li');
+            fetch(`https://api.themoviedb.org/3/tv/${movieId}?api_key=fb6a1d3f38c3d97f67df6d141f936f29&language=en-US&format=json`)
+              .then(function (response) {
+                return response.json();
+              }).then(function (responseText) {
+                if (responseText) {
+                  let homepage = responseText.homepage;
+                  li.innerHTML = movie.original_name;
+                  a.setAttribute('href', homepage);
+                  a.append(li);
+                  let imagePath = `http://image.tmdb.org/t/p/w185/${responseText.poster_path}`;
+                  let img = document.createElement('img');
+                  img.setAttribute('src', imagePath);
+                  a.append(img);
+                  movieList.append(a);
+                }
+              })
+          })
+        }
+      });
+    }
+}
+
+
+function fireEnterKey() {
+  let input = document.getElementById('searchBar').value;
+  if (event.keyCode == 13 && input)
+    document.getElementById('searchButton').click();
+}
